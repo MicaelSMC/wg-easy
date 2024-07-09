cbda38298bdb1b7a78e072e0248101fd1a92d1f9
MicaelSMC <micael.simao@smartconnect.pt>
2024-07-09 16:03:43 +0100

    fix

--- src/lib/WireGuard.js
+++ src/lib/WireGuard.js
@@ -56,6 +56,7 @@ module.exports = class WireGuard {
               privateKey,
               publicKey,
               address,
+              cidrSubnet,
             },
             clients: {},
           };
