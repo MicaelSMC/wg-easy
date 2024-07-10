30666a2a6c7585b5a55b1f44b21f816850435305
MicaelSMC <micael.simao@smartconnect.pt>
2024-07-10 11:22:41 +0100

    fix

--- src/lib/WireGuard.js
+++ src/lib/WireGuard.js
@@ -248,24 +248,24 @@ Endpoint = ${WG_HOST}:${WG_CONFIG_PORT}`;
 
     // Calculate next IP
     let address;
-    for (let j =0; j < 255; j++) {
+    
       for (let i = 2; i < 255; i++) {
         const client = Object.values(config.clients).find(client => {
           const tempAddress = config.server.address.split('.');
-          tempAddress[2] = j;
+          
           tempAddress[3] = i;
           return client.address === tempAddress.join('.');
         });
   
         if (!client) {
           const tempAddress = config.server.address.split('.');
-          tempAddress[2] = j;
+         
           tempAddress[3] = i;
           address = tempAddress.join('.');
           break;
         }
       }
-    }
+    
     
 
     if (!address) {
